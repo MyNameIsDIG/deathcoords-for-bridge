8fb187a409b27d31746c4d863a446ff22142c5da
MyNameIsDIG <37330429+MyNameIsDIG@users.noreply.github.com>
2023-09-28 00:14:12 +0800

    Create deathCoords.js

---
+++ deathCoords.js
@@ -0,0 +1,24 @@
+/**
+ * Death Coordinates (deathCoords) - BedrockBridge Plugin
+ * 
+ * This bridge-addon provides players with their death coordinates both in-game and on Discord
+ * 
+ * by MyNameIsDIG (https://github.com/MyNameIsDIG)
+ */
+
+import { bridge } from '../addons'
+
+bridge.events.playerDieLog.subscribe((e, player) => {
+	let { x, y, z } = player.location;
+	x = Math.floor(x);
+	y = Math.floor(y);
+	z = Math.floor(z);
+
+	const dimension = player.dimension.id.slice('minecraft:'.length);
+
+	// In-game
+	player.sendMessage(`You died in ${dimension !== 'end' ? `the ${dimension}` : dimension} at ${x}, ${y}, ${z}`);
+
+	// Discord
+	e.message += ` in ${dimension !== 'end' ? `the ${dimension}` : dimension} at ${x}, ${y}, ${z}`;
+});
